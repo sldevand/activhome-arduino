9920a0c3017f2e295a35845657d65c2f3ccc71bd
sldevand <sldeveloperand@gmail.com>
2023-12-29 01:44:02 +0100

    remove compareReceivedSignals + format  ChaconDIO library

--- libraries/ActivLib/ChaconDIO.cpp
+++ libraries/ActivLib/ChaconDIO.cpp
@@ -1,339 +1,277 @@
 #include "ChaconDIO.h"
 
-ChaconDIO::ChaconDIO()
-{
+ChaconDIO::ChaconDIO() {}
+
+ChaconDIO::ChaconDIO(uint8_t p_pinTx, uint8_t p_pinRx, unsigned long p_sender) {
+  setParameters(p_pinTx, p_pinRx, p_sender);
 }
 
-ChaconDIO::ChaconDIO(uint8_t p_pinTx, uint8_t p_pinRx, unsigned long p_sender)
-{
-	setParameters(p_pinTx, p_pinRx, p_sender);
-}
+ChaconDIO::~ChaconDIO() {}
 
-ChaconDIO::~ChaconDIO()
-{
-}
+void ChaconDIO::setParameters(uint8_t p_pinTx, uint8_t p_pinRx,
+                              unsigned long p_sender) {
+  pinTx = p_pinTx;
+  pinRx = p_pinRx;
+  m_sender = p_sender;
+  pinMode(pinTx, OUTPUT);
+  digitalWrite(pinTx, LOW);
+  pinMode(pinRx, INPUT);
 
-void ChaconDIO::setParameters(uint8_t p_pinTx, uint8_t p_pinRx, unsigned long p_sender)
-{
-	pinTx = p_pinTx;
-	pinRx = p_pinRx;
-	m_sender = p_sender;
-	pinMode(pinTx, OUTPUT);
-	digitalWrite(pinTx, LOW);
-	pinMode(pinRx, INPUT);
+  Serial.begin(9600);
+  Serial.println(F("init Chacon"));
+  coeffDelay = 1.0f;
 
-	Serial.begin(9600);
-	Serial.println(F("init Chacon"));
-	coeffDelay = 1.0f;
-
-	increment = 0.01f;
-	coeffDelayRcv = 1.0f;
-	tries = 0;
+  increment = 0.01f;
+  coeffDelayRcv = 1.0f;
+  tries = 0;
 
 #ifdef LISTENER_MODE
-	receivedSignal.sender = 0;
-	receivedSignal.receptor = 0;
-	receivedSignal.isSignal = false;
+  receivedSignal.sender = 0;
+  receivedSignal.receptor = 0;
+  receivedSignal.isSignal = false;
 
-	previousReceivedSignal.sender = 0;
-	previousReceivedSignal.receptor = 0;
-	previousReceivedSignal.isSignal = false;
+  previousReceivedSignal.sender = 0;
+  previousReceivedSignal.receptor = 0;
+  previousReceivedSignal.isSignal = false;
 #endif
 }
 
-void ChaconDIO::go(int interruptor, byte onoff)
-{
-	m_interruptor = interruptor;
-	itob(m_sender, 26); // conversion du code de l'emetteur (ici 8217034) en code binaire
-	itobInterruptor(interruptor, 4);
+void ChaconDIO::go(int interruptor, byte onoff) {
+  m_interruptor = interruptor;
+  itob(m_sender,
+       26); // conversion du code de l'emetteur (ici 8217034) en code binaire
+  itobInterruptor(interruptor, 4);
 
-	int retries = 5;
-	for (int i = 0; i < retries; i++)
-	{
-		transmit((bool)onoff);
-	}
+  int retries = 5;
+  for (int i = 0; i < retries; i++) {
+    transmit((bool)onoff);
+  }
 }
 
-void ChaconDIO::go(long sender, int interruptor, byte onoff)
-{
-	m_sender = sender;
-	go(interruptor, onoff);
+void ChaconDIO::go(long sender, int interruptor, byte onoff) {
+  m_sender = sender;
+  go(interruptor, onoff);
 }
 
-//Envoi d'une pulsation (passage de l'etat haut a l'etat bas)
-//1 = 310µs haut puis 1340µs bas
-//0 = 310µs haut puis 310µs bas
-void ChaconDIO::sendBit(bool b)
-{
-	if (b)
-	{
-		digitalWrite(pinTx, HIGH);
-		delayMicroseconds(275 * coeffDelay); //default value 275.
-		digitalWrite(pinTx, LOW);
-		delayMicroseconds(1275 * coeffDelay); //default value 1225.
-	}
-	else
-	{
-		digitalWrite(pinTx, HIGH);
-		delayMicroseconds(275 * coeffDelay); //default value 275.
-		digitalWrite(pinTx, LOW);
-		delayMicroseconds(275 * coeffDelay); //default value 275.
-	}
+// Envoi d'une pulsation (passage de l'etat haut a l'etat bas)
+// 1 = 310µs haut puis 1340µs bas
+// 0 = 310µs haut puis 310µs bas
+void ChaconDIO::sendBit(bool b) {
+  if (b) {
+    digitalWrite(pinTx, HIGH);
+    delayMicroseconds(275 * coeffDelay); // default value 275.
+    digitalWrite(pinTx, LOW);
+    delayMicroseconds(1275 * coeffDelay); // default value 1225.
+  } else {
+    digitalWrite(pinTx, HIGH);
+    delayMicroseconds(275 * coeffDelay); // default value 275.
+    digitalWrite(pinTx, LOW);
+    delayMicroseconds(275 * coeffDelay); // default value 275.
+  }
 }
 
-//Calcul le nombre 2^chiffre indiqué, fonction utilisée par itob pour la conversion decimal/binaire
-unsigned long ChaconDIO::power2(int power)
-{
-	unsigned long integer = 1;
-	for (int i = 0; i < power; i++)
-	{
-		integer *= 2;
-	}
-	return integer;
+// Calcul le nombre 2^chiffre indiqué, fonction utilisée par itob pour la
+// conversion decimal/binaire
+unsigned long ChaconDIO::power2(int power) {
+  unsigned long integer = 1;
+  for (int i = 0; i < power; i++) {
+    integer *= 2;
+  }
+  return integer;
 }
 
-//Convertit un nombre en binaire, nécessite le nombre, et le nombre de bits souhaités en sortie (ici 26)
-// Stocke le résultat dans le tableau global "bit2"
-void ChaconDIO::itob(unsigned long integer, int length)
-{
-	for (int i = 0; i < length; i++)
-	{
-		if ((integer / power2(length - 1 - i)) == 1)
-		{
-			integer -= power2(length - 1 - i);
-			bit2[i] = 1;
-		}
-		else
-			bit2[i] = 0;
-	}
+// Convertit un nombre en binaire, nécessite le nombre, et le nombre de bits
+// souhaités en sortie (ici 26)
+//  Stocke le résultat dans le tableau global "bit2"
+void ChaconDIO::itob(unsigned long integer, int length) {
+  for (int i = 0; i < length; i++) {
+    if ((integer / power2(length - 1 - i)) == 1) {
+      integer -= power2(length - 1 - i);
+      bit2[i] = 1;
+    } else
+      bit2[i] = 0;
+  }
 }
 
-void ChaconDIO::itobInterruptor(unsigned long integer, int length)
-{
-	for (int i = 0; i < length; i++)
-	{
-		if ((integer / power2(length - 1 - i)) == 1)
-		{
-			integer -= power2(length - 1 - i);
-			bit2Interruptor[i] = 1;
-		}
-		else
-			bit2Interruptor[i] = 0;
-	}
+void ChaconDIO::itobInterruptor(unsigned long integer, int length) {
+  for (int i = 0; i < length; i++) {
+    if ((integer / power2(length - 1 - i)) == 1) {
+      integer -= power2(length - 1 - i);
+      bit2Interruptor[i] = 1;
+    } else
+      bit2Interruptor[i] = 0;
+  }
 }
 
-//Envoi d'une paire de pulsation radio qui definissent 1 bit réel : 0 =01 et 1 =10
-//c'est le codage Manchester, il permet entre autres de dissocier les données des parasites
-void ChaconDIO::sendPair(bool b)
-{
-	if (b)
-	{
-		sendBit(true);
-		sendBit(false);
-	}
-	else
-	{
-		sendBit(false);
-		sendBit(true);
-	}
+// Envoi d'une paire de pulsation radio qui definissent 1 bit réel : 0 =01 et 1
+// =10 c'est le codage Manchester, il permet entre autres de dissocier les
+// données des parasites
+void ChaconDIO::sendPair(bool b) {
+  if (b) {
+    sendBit(true);
+    sendBit(false);
+  } else {
+    sendBit(false);
+    sendBit(true);
+  }
 }
 
-//Fonction d'envoi du signal
-//recoit en parametre un booleen définissant l'arret ou la marche du matos (true = on, false = off)
-void ChaconDIO::transmit(int blnOn)
-{
-	int i;
+// Fonction d'envoi du signal
+// recoit en parametre un booleen définissant l'arret ou la marche du matos
+// (true = on, false = off)
+void ChaconDIO::transmit(int blnOn) {
+  int i;
 
-	// Sequence de verrou anoncant le départ du signal au recepeteur
-	digitalWrite(pinTx, HIGH);
-	delayMicroseconds(275 * coeffDelay); // un bit de bruit avant de commencer pour remettre les delais du recepteur a 0
-	digitalWrite(pinTx, LOW);
-	delayMicroseconds(9900 * coeffDelay); // premier verrou de 9900µs*/
-	digitalWrite(pinTx, HIGH);			  // high again
-	delayMicroseconds(275 * coeffDelay);  // attente de 275µs entre les deux verrous
-	digitalWrite(pinTx, LOW);			  // second verrou de 2675µs
-	delayMicroseconds(2675 * coeffDelay);
-	digitalWrite(pinTx, HIGH); // On reviens en état haut pour bien couper les verrous des données
+  // Sequence de verrou anoncant le départ du signal au recepeteur
+  digitalWrite(pinTx, HIGH);
+  delayMicroseconds(275 *
+                    coeffDelay); // un bit de bruit avant de commencer pour
+                                 // remettre les delais du recepteur a 0
+  digitalWrite(pinTx, LOW);
+  delayMicroseconds(9900 * coeffDelay); // premier verrou de 9900µs*/
+  digitalWrite(pinTx, HIGH);            // high again
+  delayMicroseconds(275 *
+                    coeffDelay); // attente de 275µs entre les deux verrous
+  digitalWrite(pinTx, LOW);      // second verrou de 2675µs
+  delayMicroseconds(2675 * coeffDelay);
+  digitalWrite(
+      pinTx,
+      HIGH); // On reviens en état haut pour bien couper les verrous des données
 
-	// Envoie du code emetteur (272946 = 1000010101000110010  en binaire)
-	for (i = 0; i < 26; i++)
-	{
-		sendPair(bit2[i]);
-	}
+  // Envoie du code emetteur (272946 = 1000010101000110010  en binaire)
+  for (i = 0; i < 26; i++) {
+    sendPair(bit2[i]);
+  }
 
-	// Envoie du bit définissant si c'est une commande de groupe ou non (26em bit)
-	sendPair(false);
+  // Envoie du bit définissant si c'est une commande de groupe ou non (26em bit)
+  sendPair(false);
 
-	// Envoie du bit définissant si c'est allumé ou eteint 27em bit)
-	sendPair(blnOn);
+  // Envoie du bit définissant si c'est allumé ou eteint 27em bit)
+  sendPair(blnOn);
 
-	// Envoie des 4 derniers bits, qui représentent le code interrupteur, ici 0 (encode sur 4 bit donc 0000)
-	// nb: sur  les télécommandes officielle chacon, les interrupteurs sont logiquement nommés de 0 à x
-	// interrupteur 1 = 0 (donc 0000) , interrupteur 2 = 1 (1000) , interrupteur 3 = 2 (0100) etc...
-	for (i = 0; i < 4; i++)
-	{
+  // Envoie des 4 derniers bits, qui représentent le code interrupteur, ici 0
+  // (encode sur 4 bit donc 0000) nb: sur  les télécommandes officielle chacon,
+  // les interrupteurs sont logiquement nommés de 0 à x interrupteur 1 = 0 (donc
+  // 0000) , interrupteur 2 = 1 (1000) , interrupteur 3 = 2 (0100) etc...
+  for (i = 0; i < 4; i++) {
 
-		if (bit2Interruptor[i] == 0)
-		{
-			sendPair(false);
-		}
-		else
-		{
-			sendPair(true);
-		}
-	}
+    if (bit2Interruptor[i] == 0) {
+      sendPair(false);
+    } else {
+      sendPair(true);
+    }
+  }
 
-	digitalWrite(pinTx, HIGH);			 // coupure données, verrou
-	delayMicroseconds(275 * coeffDelay); // attendre 275µs
-	digitalWrite(pinTx, LOW);			 // verrou 2 de 2675µs pour signaler la fermeture du signal
-										 //delayMicroseconds(2675*coeffDelay);      // attendre 275µs
+  digitalWrite(pinTx, HIGH);           // coupure données, verrou
+  delayMicroseconds(275 * coeffDelay); // attendre 275µs
+  digitalWrite(
+      pinTx, LOW); // verrou 2 de 2675µs pour signaler la fermeture du signal
+                   // delayMicroseconds(2675*coeffDelay);      // attendre 275µs
 }
 
 #ifdef LISTENER_MODE
 
-bool ChaconDIO::listenSignal()
-{
-	receivedSignal.sender = 0;
-	receivedSignal.receptor = 0;
-	receivedSignal.isSignal = false;
+bool ChaconDIO::listenSignal() {
+  receivedSignal.sender = 0;
+  receivedSignal.receptor = 0;
+  receivedSignal.isSignal = false;
 
-	int i = 0;
-	unsigned long t = 0;
-	unsigned long prevT = 0;
-	byte prevBit = 0;
-	byte bit = 0;
+  int i = 0;
+  unsigned long t = 0;
+  unsigned long prevT = 0;
+  byte prevBit = 0;
+  byte bit = 0;
 
-	unsigned long sender = 0;
-	bool group = false;
-	bool on = false;
-	unsigned int recipient = 0;
-	t = pulseIn(pinRx, LOW, 12000);
+  unsigned long sender = 0;
+  bool group = false;
+  bool on = false;
+  unsigned int recipient = 0;
+  t = pulseIn(pinRx, LOW, 12000);
 
-	//Latch1
-	if (t > coeffDelayRcv * 4000 && t < coeffDelayRcv * 11000)
-	{
-		t = pulseIn(pinRx, LOW, 4000);
+  // Latch1
+  if (t > coeffDelayRcv * 4000 && t < coeffDelayRcv * 11000) {
+    t = pulseIn(pinRx, LOW, 4000);
 
-		//Latch2
-		if (t > coeffDelayRcv * 40 && t < coeffDelayRcv * 4000)
-		{
-			// data
-			while (i < 64)
-			{
-				t = pulseIn(pinRx, LOW, 2500);
+    // Latch2
+    if (t > coeffDelayRcv * 40 && t < coeffDelayRcv * 4000) {
+      // data
+      while (i < 64) {
+        t = pulseIn(pinRx, LOW, 2500);
 
-				if (t > coeffDelayRcv * 150 && t < coeffDelayRcv * 600)
-				{
-					bit = 0;
-				}
-				else if (t > coeffDelayRcv * 900 && t < coeffDelayRcv * 1800)
-				{
-					bit = 1;
-				}
-				else
-				{
-					i = 0;
-					break;
-				}
+        if (t > coeffDelayRcv * 150 && t < coeffDelayRcv * 600) {
+          bit = 0;
+        } else if (t > coeffDelayRcv * 900 && t < coeffDelayRcv * 1800) {
+          bit = 1;
+        } else {
+          i = 0;
+          break;
+        }
 
-				if (i % 2 == 0)
-				{
-					prevT = t;
-				}
+        if (i % 2 == 0) {
+          prevT = t;
+        }
 
-				if (i % 2 == 1)
-				{
-					if ((prevBit ^ bit) == 0)
-					{ // must be either 01 or 10, cannot be 00 or 11
-						i = 0;
-						break;
-					}
+        if (i % 2 == 1) {
+          if ((prevBit ^ bit) ==
+              0) { // must be either 01 or 10, cannot be 00 or 11
+            i = 0;
+            break;
+          }
 
-					if (i < 53)
-					{ // first 26 data bits
-						sender <<= 1;
-						sender |= prevBit;
-					}
-					else if (i == 53)
-					{ // 26th data bit
-						group = prevBit;
-					}
-					else if (i == 55)
-					{ // 27th data bit
-						on = prevBit;
-					}
-					else
-					{ // last 4 data bits
-						recipient <<= 1;
-						recipient |= prevBit;
-					}
-				}
-				prevBit = bit;
-				++i;
-			}
+          if (i < 53) { // first 26 data bits
+            sender <<= 1;
+            sender |= prevBit;
+          } else if (i == 53) { // 26th data bit
+            group = prevBit;
+          } else if (i == 55) { // 27th data bit
+            on = prevBit;
+          } else { // last 4 data bits
+            recipient <<= 1;
+            recipient |= prevBit;
+          }
+        }
+        prevBit = bit;
+        ++i;
+      }
 
-			// interpret message
-			if (i > 0)
-			{
-				receivedSignal.sender = sender;
-				receivedSignal.receptor = recipient;
-				receivedSignal.isSignal = true;
-				if (on)
-				{
-					receivedSignal.state = true;
-				}
-				else
-				{
-					receivedSignal.state = false;
-				}
-			}
-		}
-	}
+      // interpret message
+      if (i > 0) {
+        receivedSignal.sender = sender;
+        receivedSignal.receptor = recipient;
+        receivedSignal.isSignal = true;
+        if (on) {
+          receivedSignal.state = true;
+        } else {
+          receivedSignal.state = false;
+        }
+      }
+    }
+  }
 
-	bool res = receivedSignal.state;
+  bool res = receivedSignal.state;
 
-	return checkSignal();
+  return checkSignal();
 }
 
-bool ChaconDIO::checkSignal()
-{
-	//Si un signal au protocol home easy est reçu...
-	if (receivedSignal.isSignal)
-	{
-		if (!compareReceivedSignals())
-		{
-			previousReceivedSignal = receivedSignal;
-			displaySignal();
-		}
-
-		return true;
-	}
-	else
-	{
-		return false;
-	}
+bool ChaconDIO::checkSignal() {
+  // Si un signal au protocol home easy est reçu...
+  if (receivedSignal.isSignal) {
+    displaySignal();
+    return true;
+  } else {
+    return false;
+  }
 }
 
-bool ChaconDIO::compareReceivedSignals()
-{
-	return previousReceivedSignal.sender == receivedSignal.sender &&
-		   previousReceivedSignal.receptor == receivedSignal.receptor &&
-		   previousReceivedSignal.state == receivedSignal.state;
+void ChaconDIO::displaySignal() {
+  Serial.print("chacon-dio ");
+  Serial.print(receivedSignal.sender);
+  Serial.print(" ");
+  Serial.print(receivedSignal.receptor);
+  Serial.print(" ");
+  Serial.println(receivedSignal.state);
 }
 
-void ChaconDIO::displaySignal()
-{
-	Serial.print("chacon-dio ");
-	Serial.print(receivedSignal.sender);
-	Serial.print(" ");
-	Serial.print(receivedSignal.receptor);
-	Serial.print(" ");
-	Serial.println(receivedSignal.state);
-}
-
-unsigned long ChaconDIO::getPulse()
-{
-	return pulseIn(pinRx, LOW, 100000);
-}
+unsigned long ChaconDIO::getPulse() { return pulseIn(pinRx, LOW, 100000); }
 
 #endif
--- libraries/ActivLib/ChaconDIO.h
+++ libraries/ActivLib/ChaconDIO.h
@@ -1,62 +1,59 @@
 #ifndef CHACONDIO_H
 #define CHACONDIO_H
-#define LISTENER_MODE 
+#define LISTENER_MODE
 
 #include <Arduino.h>
 
-class ChaconDIO
-{
+class ChaconDIO {
 public:
-	/*****CTOR DTOR*****/
-	ChaconDIO();
-	ChaconDIO(uint8_t p_pinTx, uint8_t p_pinRx, unsigned long p_sender);
-	~ChaconDIO();
+  /*****CTOR DTOR*****/
+  ChaconDIO();
+  ChaconDIO(uint8_t p_pinTx, uint8_t p_pinRx, unsigned long p_sender);
+  ~ChaconDIO();
 
-	/*****METHODS*******/
+  /*****METHODS*******/
 
-	void setParameters(uint8_t p_pinTx, uint8_t p_pinRx, unsigned long p_sender);
-	void go(int interruptor, byte onoff);
-	void go(long sender, int interruptor, byte onoff);
-	void sendBit(bool b);
-	unsigned long power2(int power);
-	void itob(unsigned long integer, int length);
-	void itobInterruptor(unsigned long integer, int length);
-	void sendPair(bool b);
-	void transmit(int blnOn);
-
-#ifdef LISTENER_MODE
-	bool listenSignal();
-	bool checkSignal();
-	void displaySignal();
-	bool compareReceivedSignals();
-	unsigned long getPulse();
-#endif
-
-	/*****MEMBERS*******/
-	uint8_t pinTx;
-	uint8_t pinRx;
-	bool bit2[26]; // 26 bit Identifiant emetteur
-	bool bit2Interruptor[4];
-	int m_interruptor;
-	unsigned long m_sender;
-	float coeffDelay;
-	float coeffDelayRcv;
-	float increment;
-	float tries;
+  void setParameters(uint8_t p_pinTx, uint8_t p_pinRx, unsigned long p_sender);
+  void go(int interruptor, byte onoff);
+  void go(long sender, int interruptor, byte onoff);
+  void sendBit(bool b);
+  unsigned long power2(int power);
+  void itob(unsigned long integer, int length);
+  void itobInterruptor(unsigned long integer, int length);
+  void sendPair(bool b);
+  void transmit(int blnOn);
 
 #ifdef LISTENER_MODE
-	struct signal_t
-	{
-		long sender;
-		int receptor;
-		boolean isSignal;
-		boolean state;
-	};
-
-	signal_t receivedSignal;
-	signal_t previousReceivedSignal;
+  bool listenSignal();
+  bool checkSignal();
+  void displaySignal();
+  unsigned long getPulse();
 #endif
-	//String m_onoff;
+
+  /*****MEMBERS*******/
+  uint8_t pinTx;
+  uint8_t pinRx;
+  bool bit2[26]; // 26 bit Identifiant emetteur
+  bool bit2Interruptor[4];
+  int m_interruptor;
+  unsigned long m_sender;
+  float coeffDelay;
+  float coeffDelayRcv;
+  float increment;
+  float tries;
+
+#ifdef LISTENER_MODE
+  struct signal_t {
+    long sender;
+    int receptor;
+    boolean isSignal;
+    boolean state;
+  };
+
+  signal_t receivedSignal;
+  signal_t previousReceivedSignal;
+#endif
+  // String m_onoff;
 };
 
 #endif
